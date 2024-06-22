29a27458f296d5ddcf0bb851bccb4e50aeb74367
cbdiaz <carboad@gmail.com>
2024-06-22 16:50:53 -0400

    detailview

---
+++ tuiter/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Publicaciones
+
+# Create your views here.
+
+class HomePageView(ListView):
+    template_name = "home.html"
+    model = Publicaciones
+
+class DetailPageView(DetailView):
+    template_name = "post_detail.html"
+    model = Publicaciones
